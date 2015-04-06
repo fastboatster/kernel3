1bf4c579b4695e00a394d37899d7477e8b247e5a
yuva29 <yuva.shankar29@gmail.com>
2015-04-05 18:30:24 -0700

    kernel 1 bug rectified(status = 0)

--- kernel/proc/proc.c
+++ kernel/proc/proc.c
@@ -459,7 +459,7 @@ do_waitpid(pid_t pid, int options, int *status)
 		dbg(DBG_PRINT, "INFO : no children found, so returns -ECHILD \n");
 		if(status) {
 			dbg(DBG_PRINT, "(GRADING1C 1)\n");
-			*status = -1;
+			*status = 0;
 		}
 		return -ECHILD;
 	}
