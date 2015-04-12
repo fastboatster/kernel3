f9039bb3991bd0925266b51d3a88924935fb39e2
fastboatster <fastboatster@gmail.com>
2015-04-12 16:57:34 -0700

    vmmap_lookup

--- kernel/vm/vmmap.c
+++ kernel/vm/vmmap.c
@@ -152,7 +152,10 @@ vmmap_destroy(vmmap_t *map)
 void
 vmmap_insert(vmmap_t *map, vmarea_t *newvma)
 {
-        NOT_YET_IMPLEMENTED("VM: vmmap_insert");
+    /*do we need to iterate the list ot find a spot to insert newvma or any place is OK?*/
+	list_insert_tail(&map->vmm_list, &newvma->vma_plink);
+	newvma->vma_vmmap = map;
+	/* NOT_YET_IMPLEMENTED("VM: vmmap_insert");*/
 }
 
 /* Find a contiguous range of free virtual pages of length npages in
@@ -175,7 +178,16 @@ vmmap_find_range(vmmap_t *map, uint32_t npages, int dir)
 vmarea_t *
 vmmap_lookup(vmmap_t *map, uint32_t vfn)
 {
-        NOT_YET_IMPLEMENTED("VM: vmmap_lookup");
+       /* NOT_YET_IMPLEMENTED("VM: vmmap_lookup");*/
+	list_link_t *link;
+		for (link = (&(map->vmm_list))->l_next; link != &(map->vmm_list); link = link->l_next) {
+			vmarea_t* area = list_item(link, vmarea_t, vma_plink);
+	        uint32_t vma_start = area->vma_start;
+	        uint32_t vma_end = area->vma_end;
+			if(vma_start <= vfn && vfn < vma_end) {
+				return area;
+			}
+		};
         return NULL;
 }
 
