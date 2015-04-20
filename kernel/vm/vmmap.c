e425f9c79f38058e29ae2c826ae694893ae24770
fastboatster <fastboatster@gmail.com>
2015-04-20 12:02:44 -0700

    another lost stuff fix

--- kernel/vm/vmmap.c
+++ kernel/vm/vmmap.c
@@ -358,18 +358,19 @@ int vmmap_map(vmmap_t *map, vnode_t *file, uint32_t lopage, uint32_t npages,
 			vmmap_remove(map, lopage, npages);
 			new_area->vma_start = lopage;
 			new_area->vma_end = npages + lopage; /* what about 4KB ?? */
-			if(file) {
+		vmmap_insert(map, new_area);
+		if(file) {
 				int mmobj_ret = file->vn_ops->mmap(file, new_area, &new_area->vma_obj);
 				if(mmobj_ret >= 0) {
 					new_area->vma_obj->mmo_ops->ref(new_area->vma_obj);
 					vref(file);
 				}
-			} else if(flags & MAP_PRIVATE) {
+			}/* else if(flags & MAP_PRIVATE) {
 				new_area->vma_obj =  shadow_create();
-			} else { /* anon obj */
+			} */else { /* anon obj */
 				new_area->vma_obj =  anon_create();
 			}
-			vmmap_insert(map, new_area);
+
 			new_area->vma_vmmap = map;
 			new = &new_area;
 			return 0;
