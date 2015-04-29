b226658c3db34738352d23a4cd6914a4e5496da0
fastboatster <fastboatster@gmail.com>
2015-04-29 13:10:59 -0700

    do_brk changed

--- kernel/vm/brk.c
+++ kernel/vm/brk.c
@@ -71,38 +71,57 @@ do_brk(void *addr, void **ret)
 {
        /* NOT_YET_IMPLEMENTED("VM: do_brk");
         return 0;*/
-        if (addr == NULL){
-        		*ret = curproc->p_brk;
-        		return 0;
-        	}
+       /*error handling*/
+	if (addr == NULL) {
+		*ret = curproc->p_brk;
+		return 0;
+	}
+	/*error handling*/
+	if ((addr < curproc->p_start_brk) || (addr > USER_MEM_HIGH)) {
+		return -ENOMEM;
+	}
+	/*quickly check if addr is inside a heap area*/
+	/*	vmarea_t * new_area = vmmap_lookup(curproc->p_vmmap, ADDR_TO_PN(curproc->p_brk));
+	 if (new_area == NULL) {This should be not be NULL
+	 return -1;  what should we return as error
+	 }
+	 if (curproc->p_start_brk < addr && addr < curproc->p_brk) {
+	 curproc->p_brk = PAGE_ALIGN_UP(addr);
+	 new_area->vma_end = ADDR_TO_PN(PAGE_ALIGN_UP(addr));
+	 *ret = curproc->p_brk;
+	 return 0;
+	 }*/
 
-        	if ((addr < curproc->p_start_brk) || (addr > USER_MEM_HIGH)){
-        		return -ENOMEM;
-        	}
-        	vmarea_t * new_area = vmmap_lookup(curproc->p_vmmap, ADDR_TO_PN(curproc->p_brk));
-        	if (new_area == NULL){/*This should be not be NULL*/
-        		return -1; /* what should we return as error*/
-        	}
-        	if (addr < curproc->p_brk){
-        		curproc->p_brk = PAGE_ALIGN_UP(addr);
-        		new_area->vma_end = ADDR_TO_PN(PAGE_ALIGN_UP(addr));
-        		*ret = curproc->p_brk;
-        		return 0;
-        	}
-        	if (addr > curproc->p_brk) {
-        		vmarea_t * area = vmmap_lookup(curproc->p_vmmap, ADDR_TO_PN(curproc->p_start_brk));
-        		if (area ==NULL) {
-        			uint32_t lopage = ADDR_TO_PN(PAGE_ALIGN_UP(curproc->p_start_brk));
-        			uint32_t numpages = ADDR_TO_PN(PAGE_ALIGN_UP(addr)) - lopage;
-        			vmmap_map(curproc->p_vmmap, NULL, lopage, numpages,PROT_READ|PROT_WRITE, MAP_PRIVATE, 0, VMMAP_DIR_HILO, NULL);
-        			curproc->p_brk = PAGE_ALIGN_UP(addr);
-        			*ret = curproc->p_brk;
-        		}else {
-        			area->vma_end = ADDR_TO_PN(PAGE_ALIGN_UP(addr));
-        			curproc->p_brk = PAGE_ALIGN_UP(addr);
-        			*ret = curproc->p_brk;
-        		}
-        	}
+	/*find the end of bss region:*/
+	vmarea_t * bss_area = vmmap_lookup(curproc->p_vmmap,
+			ADDR_TO_PN(curproc->p_start_brk));
+	/*end vfn of the bss area*/
+	uint32_t bss_end = bss_area->vma_end;
+	void* bss_end_addr = PN_TO_ADDR(bss_end);
+	uint32_t addr_vfn = ADDR_TO_PN(addr);
+	uint32_t gap = addr_vfn - bss_end; /*number of pages b-ween bss_end and addr*/
+	/*check if we already have a heap vmarea:*/
+	if (vmmap_is_range_empty(curproc->p_vmmap, bss_end, gap)) {
+		/* heap vmarea doesn't exist, allocate a new one*/
+		uint32_t lopage = bss_end;
+		uint32_t numpages = ADDR_TO_PN(PAGE_ALIGN_UP(addr)) - lopage;
+		vmmap_map(curproc->p_vmmap, NULL, lopage, numpages,
+		PROT_READ | PROT_WRITE, MAP_PRIVATE, 0, VMMAP_DIR_HILO,
+		NULL);
+		curproc->p_brk = addr;
+		*ret = curproc->p_brk;
+		return 0;
+
+	} else { /*heap vmarea already exists, need to find it*/
+		vmarea_t * heap_area = vmmap_lookup(curproc->p_vmmap, bss_end); /*heap area must be right after bss, and end of bss area is included*/
+		/*now, there're several cases*/
+		/*case 1 - new addr is > curproc->p_brk*/
+		if (addr > curproc->p_brk) {
+			heap_area->vma_end = ADDR_TO_PN(PAGE_ALIGN_UP(addr));
+			curproc->p_brk = addr;
+			*ret = curproc->p_brk;
+			return 0;
+		}
         /*	uint32_t end_vfn = ADDR_TO_PN(PAGE_ALIGN_UP(addr));
         	if(end_vfn == new_area->vma_end) { if page-aligned vfn of a new start equals the end of new_area, increment it
         		end_vfn++;
